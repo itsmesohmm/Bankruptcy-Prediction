60ba3effb1075118d03b5eec21d5c3ddd87a1b61
Soham Helekar <helekarsohmm@gmail.com>
2026-06-14 12:19:34 +0530

    load buttons added

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-function loadSampleData() {
+function loadLowRisk() {
 
     document.getElementById("year").value = 2018;
 
@@ -23,4 +23,65 @@ function loadSampleData() {
     document.getElementById("market_value").value = 150000;
     document.getElementById("depreciation").value = 2000;
     document.getElementById("cogs").value = 45000;
+}
+
+
+function loadModerateRisk() {
+
+    document.getElementById("year").value = 2018;
+
+    document.getElementById("current_assets").value = 5000;
+    document.getElementById("inventory").value = 3000;
+    document.getElementById("receivables").value = 1000;
+    document.getElementById("total_assets").value = 10000;
+
+    document.getElementById("revenue").value = 5000;
+    document.getElementById("net_sales").value = 4500;
+    document.getElementById("gross_profit").value = -2000;
+
+    document.getElementById("net_income").value = -30000;
+    document.getElementById("ebit").value = -25000;
+    document.getElementById("ebitda").value = -20000;
+
+    document.getElementById("liabilities").value = 150000;
+    document.getElementById("current_liabilities").value = 70000;
+    document.getElementById("long_term_debt").value = 60000;
+
+    document.getElementById("retained_earnings").value = -50000;
+
+    document.getElementById("operating_expenses").value = 35000;
+
+    document.getElementById("market_value").value = 1000;
+
+    document.getElementById("depreciation").value = 500;
+
+    document.getElementById("cogs").value = 12000;
+}
+
+
+function loadHighRisk() {
+
+    document.getElementById("year").value = 2018;
+
+    document.getElementById("current_assets").value = 1000;
+    document.getElementById("inventory").value = 500;
+    document.getElementById("receivables").value = 200;
+    document.getElementById("total_assets").value = 5000;
+
+    document.getElementById("revenue").value = 1000;
+    document.getElementById("net_sales").value = 800;
+    document.getElementById("gross_profit").value = -5000;
+    document.getElementById("net_income").value = -50000;
+    document.getElementById("ebit").value = -45000;
+    document.getElementById("ebitda").value = -40000;
+
+    document.getElementById("liabilities").value = 300000;
+    document.getElementById("current_liabilities").value = 150000;
+    document.getElementById("long_term_debt").value = 100000;
+
+    document.getElementById("retained_earnings").value = -100000;
+    document.getElementById("operating_expenses").value = 60000;
+    document.getElementById("market_value").value = 100;
+    document.getElementById("depreciation").value = 1000;
+    document.getElementById("cogs").value = 20000;
 }
